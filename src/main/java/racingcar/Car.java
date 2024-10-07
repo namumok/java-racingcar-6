d809573cfeb0fb9369c5167d69c12f57b6bb76bb
namumok <reverse2774@gmail.com>
2024-10-07 14:08:09 +0900

    feat(Car): create Car class and implement movement logic

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,30 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0; // 초기 위치
+    }
+
+    public void drive() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        if (randomValue >= 4) {
+            this.position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+
 }
